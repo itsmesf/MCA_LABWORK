5c40f8dba6b8ebf6c01868a9ad9372017f28eb04
Sadaf Fatima <gk6762@myamu.ac.in>
2024-11-13 15:55:32 +0530

    Update Display_Matrix_using_Recursion.cpp

--- SEM_1/sessional_ques/Display_Matrix_using_Recursion.cpp
+++ SEM_1/sessional_ques/Display_Matrix_using_Recursion.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+const int rows = 3;
+const int cols = 3;
+
+void displayMatrix(int matrix[rows][cols], int r, in c){
+
+}
 int main(){
 
-int matrix[rows][cols];
-
+int matrix[rows][cols] = {{1,2,3},{4,5,6},{7,8,9}};
+displayMatrix(matrix,0,0);
 return 0;
 }
