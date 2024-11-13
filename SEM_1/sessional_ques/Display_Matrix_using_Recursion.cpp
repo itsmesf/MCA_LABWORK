7b9b69b9dbf891d81d7dde74b19b31e435d43311
Sadaf Fatima <gk6762@myamu.ac.in>
2024-11-13 16:05:15 +0530

    Update Display_Matrix_using_Recursion.cpp

--- SEM_1/sessional_ques/Display_Matrix_using_Recursion.cpp
+++ SEM_1/sessional_ques/Display_Matrix_using_Recursion.cpp
@@ -4,24 +4,29 @@ using namespace std;
 const int rows = 3;
 const int cols = 3;
 
-void displayMatrix(int matrix[rows][cols], int r, int c){
+void displayMatrix(int matrix[rows][cols], int r, int c) {
 
-if(r == rows) return;
+	if(r == rows) return;
 
-if (c == cols){
-cout<< endl;
-displayMatrix(matrix,r+1,0);
-return;
+	if (c == cols) {
+		cout<< endl;
+		displayMatrix(matrix,r+1,0);
+		return;
+	}
+
+	cout<< matrix[r][c]<<"  ";
+
+	return displayMatrix(matrix,r,c+1);
+
 }
 
-cout<< matrix[r][c];
+int main() {
 
-return displayMatrix(matrix,r,cols+1);
+	int matrix[rows][cols] = {{1,2,3}, {4,5,6},
+{7,8,9}
+};
 
-}
-int main(){
+	displayMatrix(matrix,0,0);
 
-int matrix[rows][cols] = {{1,2,3},{4,5,6},{7,8,9}};
-displayMatrix(matrix,0,0);
-return 0;
+	return 0;
 }
