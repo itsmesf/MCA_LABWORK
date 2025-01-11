682fe6691b187d0b4ac3a2e48e04179d8e53e6b2
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-11 21:26:41 +0530

    Create w2_q1.java

---
+++ SEM_2/week2/w2_q1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class w2_q1 {
+  public static void main(String args[]) {
+    
+    double num1, num2, sum , diff, product, quotient, min, max;
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter two numbers: ");
+    num1 = input.nextDouble();
+    num2 = input.nextDouble();
+    
+    //Operations on numbers
+    
+    sum = num1 + num2;
+    diff = num1 - num2;
+    product = num1 * num2;
+    quotient = (num2 == 0)? Double.NaN : num1/num2;
+    max = (num1 > num2)? num1 : num2;
+    min = (num1 == max) ? num2 : num1;
+    
+    System.out.println("Sum of numbers = " + sum);
+    System.out.println("Difference of numbers  = " + diff);
+    System.out.println("Product of numbers  = " + product);
+    System.out.printf("Quotient of numbers  = %.2f%n" ,quotient);
+    System.out.println("Maximum of numbers = " + max);
+    System.out.println("Minimum of numbers = " + min);
+  }
+}
