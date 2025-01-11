2601aeb0bfa7b736bc5fe48d363eacd51a395755
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-11 13:40:31 +0530

    Update w1_q2.java

--- SEM_2/week1/w1_q2.java
+++ SEM_2/week1/w1_q2.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-public class address{
-      static String country = "India";
+public class w1_q2{
+      
+    static String country = "India";
+      
     public static void main(String[] args){
         
         String name, house_no, street, locality, city, pincode, state;
