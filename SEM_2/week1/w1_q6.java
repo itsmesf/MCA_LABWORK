1cb2d1b1991154eeb7d25629804f57aba951a102
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-18 20:32:57 +0530

    Create w1_q6.java

---
+++ SEM_2/week1/w1_q6.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class w1_q6 {
+    
+    static int Round(double num){
+    int intNum = (int)num;
+    if(num >= 0){
+        return ((double)intNum + 0.5 <= num && (double)intNum != num) ? intNum + 1 : intNum;
+    }
+    else{
+        return((double)intNum - 0.5 >num && (double)intNum != num) ? intNum-1 : intNum;
+        }
+    }
+
+    static double Ceil(double num){
+        int intNum = (int)num;
+        return (num > intNum)? (double)intNum+1 : (double)intNum;
+    }
+
+    static double Floor(double num){
+        int intNum = (int)num;
+        return (intNum < 0 && num != intNum)? (double)intNum-1 : (double)intNum;
+    }
+    
+    static int integer(double num){
+        return (int)num;
+    }
+
+  public static void main(String args[]) {
+    
+    double num, numCeil, numFloor;
+    int numInteger, numRound;
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter a number: ");
+    num = input.nextDouble();
+    
+    //Operations on numbers
+    
+    numRound = Round(num);
+    numCeil = Ceil(num);
+    numFloor = Floor(num);
+    numInteger = integer(num);
+    
+    System.out.println("\nNumber after rounding  = " + numRound);
+    System.out.println("Ceil value of a number  = " + numCeil);
+    System.out.println("Floor value of a number  = " + numFloor);
+    System.out.println("Conversion of double to integer  = " +numInteger);
+  }
+}
