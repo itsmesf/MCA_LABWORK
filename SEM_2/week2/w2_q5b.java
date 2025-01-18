6db702bbbbf2675a994df27c3a17f9260f48d8f8
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-18 22:10:45 +0530

    Create w2_q5b.java

---
+++ SEM_2/week2/w2_q5b.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class w2_q2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num,sum = 0;
+        
+        System.out.print("Enter number of rows to be printed in Floyd's Triangle: ");
+        num = input.nextInt();
+        
+        for(int i = 1; i<=num; i++){
+            for(int j = 0; j<i ; j++){
+                sum++;
+                if(sum>1 ){
+                    sum = 0;
+                }
+                System.out.print(sum + "  ");
+            }
+            if (i%2==0){
+                sum = 0;
+            }
+            System.out.println();
+        }
+    }
+}
