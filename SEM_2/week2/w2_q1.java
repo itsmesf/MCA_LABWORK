47a9784a249c33f81299bd6fd830f90c912a301a
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-18 20:52:54 +0530

    Update w2_q1.java

--- SEM_2/week2/w2_q1.java
+++ SEM_2/week2/w2_q1.java
@@ -1,30 +1,24 @@
-
 import java.util.Scanner;
-
-public class w2_q1 {
-  public static void main(String args[]) {
+public class w2_q1{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        int even = 0,odd = 0,num;
+        
+        System.out.println("Enter numbers (-1 to exit): ");
+        
+        do{
+           num = input.nextInt();
+           
+           if(num%2==0) even++;
+           else odd++;
+           
+        }
+        while(num != -1);
+        
+        System.out.println("\nEven Count: " + even);
+        System.out.println("Odd Count: " + odd);
     
-    double num1, num2, sum , diff, product, quotient, min, max;
-    Scanner input = new Scanner(System.in);
-    
-    System.out.print("Enter two numbers: ");
-    num1 = input.nextDouble();
-    num2 = input.nextDouble();
-    
-    //Operations on numbers
-    
-    sum = num1 + num2;
-    diff = num1 - num2;
-    product = num1 * num2;
-    quotient = (num2 == 0)? Double.NaN : num1/num2;
-    max = (num1 > num2)? num1 : num2;
-    min = (num1 == max) ? num2 : num1;
-    
-    System.out.println("Sum of numbers = " + sum);
-    System.out.println("Difference of numbers  = " + diff);
-    System.out.println("Product of numbers  = " + product);
-    System.out.printf("Quotient of numbers  = %.2f%n" ,quotient);
-    System.out.println("Maximum of numbers = " + max);
-    System.out.println("Minimum of numbers = " + min);
-  }
+    }
 }
