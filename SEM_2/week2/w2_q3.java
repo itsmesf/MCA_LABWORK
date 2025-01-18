fe3c67362fac984371e8281123e2f02fcaadffc3
Sadaf Fatima <gk6762@myamu.ac.in>
2025-01-18 21:09:03 +0530

    Create w2_q3.java

---
+++ SEM_2/week2/w2_q3.java
@@ -0,0 +1,11 @@
+public class w2_q3{
+    public static void main(String[] args){
+        double sum = 0;
+        
+        for(int i = 1; i<=10; i++){
+            sum+=(1/(double)i);   
+        }
+
+        System.out.println("\nSum of reciporcals: "+ Math.round(sum*100.0) /100.0);
+    }
+}
