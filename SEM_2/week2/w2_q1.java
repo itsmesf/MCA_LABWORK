ade8df58d2f29d15a4d2788b54ae01fd2a2b559b
Sadaf Fatima <gk6762@myamu.ac.in>
2025-02-08 19:32:42 +0530

    Update w2_q1.java

--- SEM_2/week2/w2_q1.java
+++ SEM_2/week2/w2_q1.java
@@ -4,19 +4,15 @@ public class w2_q1{
         
         Scanner input = new Scanner(System.in);
         
-        int even = 0,odd = 0,num;
+        int even = 0,odd = 0,num = 0;
         
         System.out.println("Enter numbers (-1 to exit): ");
         
-        do{
+        while(num != -1){
            num = input.nextInt();
-           
            if(num%2==0) even++;
-           else odd++;
-           
+           else odd++; 
         }
-        while(num != -1);
-        
         System.out.println("\nEven Count: " + even);
         System.out.println("Odd Count: " + odd);
     
